5b5a79c10b2bcd90b41183ff4a89cefb92cc859c
inho3213 <61544239+inho3213@users.noreply.github.com>
2022-10-18 21:21:52 +0900

    JS home contact btn scroll

--- main.js
+++ main.js
@@ -22,6 +22,18 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null){
         return;
     }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+//contact me button -> scrolling
+
+const contactBtn = document.querySelector('.home__contact');
+
+contactBtn.addEventListener('click', ()=>{
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
